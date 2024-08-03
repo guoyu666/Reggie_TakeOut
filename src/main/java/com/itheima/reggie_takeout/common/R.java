c05cbf23c8ff1b154053102a2605ae17f43bc7f4
GuoYu <1262425287@qq.com>
2024-08-03 20:42:11 +0800

    提交代码，部分代码进行修改

---
+++ src/main/java/com/itheima/reggie_takeout/common/R.java
@@ -0,0 +1,41 @@
+package com.itheima.reggie_takeout.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 通用返回结果，服务端响应的数据最终都会封装成此对象
+ * @param <T>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class R<T> {
+    private Integer code;  // 编码：1成功。0和其他数字失败
+    private String errMsg;  // 错误信息
+    private T data; // 数据
+    private Map map = new HashMap();  // 动态数据
+
+    public static <T> R<T> success(T data) {
+        R<T> r = new R<>();
+        r.code = 1;  //成功状态码
+        r.data = data;
+        return r;
+    }
+
+    public static <T> R<T> error(String errMsg) {
+        R<T> r = new R<>();
+        r.errMsg = errMsg; //设置错误信息
+        r.code = 0;  //默认失败状态码，后期我们可以根据自己的需求来设置其他状态码
+        return r;
+    }
+
+    public R<T> add(String msg, String value) {
+        this.map.put(msg, value);
+        return this;
+    }
+}
---
+++ src/main/java/com/itheima/reggie_takeout/controller/EmployeeController.java
@@ -0,0 +1,71 @@
+package com.itheima.reggie_takeout.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie_takeout.common.R;
+import com.itheima.reggie_takeout.entity.Employee;
+import com.itheima.reggie_takeout.service.EmployeeService;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Resource
+    private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     * @param request 如果登陆成功把对象放入Session中，方便后续拿取
+     * @param employee 利用@RequestBody注解来解析前端传来的Json，同时用对象来封装
+     * @return
+     */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        /**
+         * 1. 将页面提交的密码password进行md5加密
+         * 2. 根据页面提交的用户名username查询数据库
+         * 3. 如果没有查询到，返回错误信息
+         * 4，密码比对，如果不一致，返回错误信息
+         * 5. 查询员工状态，如果为禁用，返回员工已禁用结果
+         * 6. 登录成功，将员工对象放入Session中，返回成功信息
+         */
+        // 1. 将页面提交的密码password进行md5加密
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        // 2. 根据页面提交的用户名username查询数据库
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        // 3. 如果没有查询到，返回错误信息
+        if(emp == null){
+            return R.error("登陆失败！！");
+        }
+
+        // 4. 密码比对，如果不一致，返回错误信息
+        if(!emp.getPassword().equals(password)){
+            return R.error("登陆失败！！");
+        }
+
+        // 5. 查询员工状态，如果为禁用，返回员工已禁用结果
+        if(emp.getStatus() == 0){
+            return R.error("员工账号已禁用！！");
+        }
+
+        // 6. 登录成功，将员工ID放入Session中，返回成功信息
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
+    }
+
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功！");
+    }
+}
