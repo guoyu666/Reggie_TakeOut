5815793758271bd53ed1c8bc2b66731b831e48c3
GuoYu <1262425287@qq.com>
2024-08-10 00:51:36 +0800

    修改部分注释

--- src/main/java/com/itheima/reggie_takeout/service/EmployeeService.java
+++ src/main/java/com/itheima/reggie_takeout/service/EmployeeService.java
@@ -3,5 +3,5 @@ package com.itheima.reggie_takeout.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.itheima.reggie_takeout.entity.Employee;
 
-public interface EmployeeService extends IService<Employee> {
+public interface EmployeeService extends IService<Employee> {   // 继承IService接口
 }
