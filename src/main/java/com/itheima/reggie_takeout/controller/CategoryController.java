0273246b436f85c677357a052f0610debb43adf0
GuoYu <1262425287@qq.com>
2024-08-13 00:24:18 +0800

    添加分页查询

---
+++ src/main/java/com/itheima/reggie_takeout/controller/CategoryController.java
@@ -0,0 +1,55 @@
+package com.itheima.reggie_takeout.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie_takeout.common.R;
+import com.itheima.reggie_takeout.entity.Category;
+import com.itheima.reggie_takeout.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 分类的管理
+ */
+@RestController
+@Slf4j
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+
+        log.info("新增分类：{}", category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        // 分页构造器
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+        // 条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        // 添加排序条件,根据sort排序
+        queryWrapper.orderByAsc(Category::getSort);
+        // 执行分页查询
+        categoryService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+
+    }
+}
